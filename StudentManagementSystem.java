a9244d1f266237ae94050edc7e4b77fc517d865b
sejaldawer2005 <150130407+sejaldawer2005@users.noreply.github.com>
2026-03-10 11:42:27 +0530

    StudentManagementSystem.java

--- StudentManagementSystem.java
+++ StudentManagementSystem.java
@@ -1,23 +1,5 @@
 import java.util.*;
-# Student Management System (Java)
 
-A console-based student management system developed using Java.
-
-## Features
-- Add student
-- Display students
-- Search student by ID
-
-## Technologies
-- Java
-- OOP concepts
-- ArrayList
-- Scanner class
-
-## Author
-Sejal Dawer  
-B.Tech Computer Science  
-Amity University Noida
 
 class Student {
     int id;
@@ -108,3 +90,4 @@ public class StudentManagementSystem {
     }
 
 }
+
